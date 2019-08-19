08adbee314ce9a54e49452884bd40780ec4463ef
RyotArch <bboy.ryu@icloud.com>
2019-08-18 21:19:55 -0700

    Making our code testable

--- sagas.js
+++ sagas.js
@@ -1,14 +1,16 @@
-import { put, takeEvery, all } from 'redux-saga/effects'
+import { put, takeEvery, all, call } from 'redux-saga/effects'
 
-const delay = (ms) => new Promise(res => setTimeout(res, ms))
+export const delay = (ms) => new Promise(res => setTimeout(res, ms))
 
 function* helloSaga() {
   console.log('Hello Sagas!')
 }
 
-function* incrementAsync() {
-  yield delay(1000)
+export function* incrementAsync() {
+  // use the call Effect
+  yield call(delay, 1000)
   yield put({ type: 'INCREMENT' })
+
 }
 
 function* watchIncrementAsync() {
